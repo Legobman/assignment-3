4f987fed406f91ce23404ac8be956d04704aac57
Matthew Berwick <msberwick@uncg.edu>
2026-02-15 01:08:22 -0500

    setup before researching how to remove items from the favorites list

--- script.js
+++ script.js
@@ -7,33 +7,76 @@ const dish_names = ["Crazy Tacos", "Burritos Rancheros", "Texanas", "Where Art T
 let total = 0;
 
 // create the favorites list
-const favoritesList = document.querySelector("footer");
-const flist = document.createElement("p");
-flist.style.textAlign = "center";
-flist.textContent = "Favorites:";
-favoritesList.parentNode.insertBefore(flist, favoritesList);
+const footer = document.querySelector("footer");
+const favoritelist = document.createElement("div");
+const favoritetitle = document.createElement("p");
+const favoritetotal = document.createElement("p");
+favoritelist.style.textAlign = "center";
+favoritetitle.textContent = "Favorites:";
+favoritetotal.textContent = "Current Total: $0.00"
+favoritelist.appendChild(favoritetitle);
+favoritetitle.appendChild(favoritetotal);
+footer.parentNode.insertBefore(favoritelist, footer);
 
 // create the functionality for the buttons
 for(let i = 0; i < dishes.length; i++){
+    // variables for the data set
     const price = prices[i];
     const dname = dish_names[i];
+    
+    // variables for the button and price tag
     const btn = document.createElement("button");
     const tag = document.createElement("p");
     btn.textContent = "Add to Favorites";
     tag.textContent = "$ " + prices[i].toFixed(2);
+    
+    // add data to the button's dataset
     btn.dataset.price = price;
     btn.dataset.dname = dname;
+
+    // add the button and price tag to the page
     dishes[i].appendChild(btn);
     dishes[i].appendChild(tag);
+
+    // add the event for adding to favorites
     btn.addEventListener("click", favorites);
 }
 
 // adds the selected dish to the favorites list and adds price to running total
 function favorites(event){
+    // highlight the selected dishes image
     const fimg = event.currentTarget.parentNode.querySelector("img");
     fimg.classList.add('selected');
+    
+    // add the title of the dish to the favorites list
+    const fitem = document.createElement("p");
+    fitem.textContent = event.currentTarget.dataset.dname;
+    favoritelist.appendChild(fitem);
+    
+    // add the price of the dish to the running total of the dish
     let price = parseFloat(event.currentTarget.dataset.price);
     total += price;
-    flist.textContent = "Favorites: <br> Current Total: $" + total.toFixed(2);
-    event.currentTarget.disabled = true;
+    favoritetotal.textContent = "Current Total: $" + total.toFixed(2);
+    
+    // turn off the favorites event and enable the rmeovefavorites event
+    event.currentTarget.removeEventListener("click", favorites);
+    event.currentTarget.addEventListener("click", removefavorites);
+    event.currentTarget.textContent = "Remove from Favorites";
+}
+
+// removes the selected dish to the favorites list and subtracts price from running total
+function removefavorites(event){
+    // remove the highlight the selected dishes image
+    const fimg = event.currentTarget.parentNode.querySelector("img");
+    fimg.classList.remove('selected');
+    
+    // subtract the price of the dish to the running total of the dish
+    let price = parseFloat(event.currentTarget.dataset.price);
+    total -= price;
+    favoritetotal.textContent = "Current Total: $" + total.toFixed(2);
+    
+    // turn off the rmeovefavorites event and enable the favorites event
+    event.currentTarget.removeEventListener("click", removefavorites);
+    event.currentTarget.addEventListener("click", favorites);
+    event.currentTarget.textContent = "Add to Favorites";
 }
